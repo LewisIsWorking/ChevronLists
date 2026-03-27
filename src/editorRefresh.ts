5a89b3101ddc6a8bbc7ac5b335e57f93927845a8
Lewis <Lewisisemailing@gmail.com>
2026-03-27 06:24:36 +0000

    v24.9.0: wire colourPrefixDecorations into editorRefresh

--- src/editorRefresh.ts
+++ src/editorRefresh.ts
@@ -24,6 +24,7 @@ import { updateStickyHeader } from './stickyHeaderDecoration';
 import { applyOverdueEscalation } from './overdueEscalation';
 import { updateUrgencyDecorations } from './urgencyDecoration';
 import { updatePriorityDecorations } from './priorityDecoration';
+import { updateColourPrefixDecorations } from './colourPrefixDecoration';
 import { updateDueSoonDecorations } from './dueSoonDecoration';
 
 export interface DiagCollections {
@@ -51,6 +52,7 @@ export function refreshEditor(
     applyOverdueEscalation(editor);
     updateUrgencyDecorations(editor);
     updatePriorityDecorations(editor);
+    updateColourPrefixDecorations(editor);
     updateDueSoonDecorations(editor);
     updateBadgeDecorations(editor);
     updateGoalDecorations(editor);
